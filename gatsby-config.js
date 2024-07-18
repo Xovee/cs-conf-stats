d5d1f23e90c18655b50c38ebefdab161dd4e9f4f
Xovee Xu <xovee.xu@gmail.com>
2024-07-18 11:12:02 +0800

    add gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,20 @@ module.exports = {
     title: `Computer Science Conference Statistics`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-postcss",
+    {
+      resolve: "gatsby-plugin-google-gtag",
+      options: {
+        trackingIds: [
+            "G-YKKPH99HSJ",
+        ],
+      }
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/images/icon.png"
+      "icon": "src/images/icon.png",
     }
   }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
